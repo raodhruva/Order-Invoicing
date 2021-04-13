826379593319cb0cae2abd50c16bb47f8fe38e01
raodhruva <40784667+raodhruva@users.noreply.github.com>
2021-04-13 14:47:29 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,19 @@
 <?php
+session_start();
 //database config settings
-define('DB_Name', 'dhruva_tables');
-define('DB_User','root');
-define('DB_Password','');
+
+$user = $_SESSION["username"];
+$pass = $_SESSION["password"];
+define('DB_Name', 'augustco_invoiceSoftware');
+//define('DB_User','augustco_user1');
+//define('DB_Password','AugustCollectives123');
 define('DB_Host', '127.0.0.1');
 
 
-$con = new mysqli(DB_Host, DB_User, DB_Password, DB_Name);
+$con = new mysqli(DB_Host, $user, $pass, DB_Name);
 if ($con->connect_error) {
-	die("Connection failed");
+    header("Location: https://augustcollectives.com/");
+	
 }
-
+//header("Location: main-page.php");
 ?>
